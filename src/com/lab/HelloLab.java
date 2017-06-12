fdaf10ff746bc441dd490318928eb6a63b70205d
iliasgr <ilias.skordilis@gmail.com>
2017-06-12 12:11:13 -0400

    remote push

--- src/com/lab/HelloLab.java
+++ src/com/lab/HelloLab.java
@@ -5,7 +5,7 @@ public class HelloLab {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello snippets!");
+		System.out.println("Hello snippets!!!!");
 		
 
 	}
